d2e598f02f8837393e75b323e04bb40c90ff5845
MinerAtivos2 <banri32800@outlook.com>
2026-03-21 08:12:13 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1455,7 +1455,7 @@ class B3App {
         return `
           <tr>
             <td><strong>${item.ticker.replace('.SA', '')}</strong></td>            
-            <td>R$${item.last_close.toFixed(2)}</td>
+            <td>R$ ${item.last_close.toFixed(2)}</td>
             <td class="${cssClass}">${(deltaVal > 0 && isGainer) ? '+' : ''}${delta}% ${icon}</td>
             <td class="${volClass}">${volVal > 0 ? '+' : ''}${volPct}% ${volIcon}</td>
           </tr>
